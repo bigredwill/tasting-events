20ef1d2d81a728020f029049c76ba9243c62e36c
wills <will@simons.dev>
2024-09-29 13:43:17 -0700

    home-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
+import "./App.css";
+import Home from "./Home";
+import { MainNav } from "./components/main-nav";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface AppLayoutProps {
+  children: React.ReactNode;
 }
 
-export default App
+function App({ children }: AppLayoutProps) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <ThemeProvider>
+      <header>
+        <div className="flex-col md:flex">
+          <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+              <MainNav className="mx-6" />
+            </div>
+          </div>
+        </div>
+      </header>
+      {children}
+      <Home />
+    </ThemeProvider>
+  );
+}
+
+export default App;
